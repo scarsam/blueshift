e2fb854b983a0274bbd4706e70b52504c69df355
Sam Ojling <s.ojling@gmail.com>
2025-07-30 23:47:40 +0200

    fix: adding proxy to route agents request to correct backend

---
+++ client/functions/[...catchall].ts
@@ -0,0 +1,17 @@
+export const onRequest: PagesFunction = async ({ request }) => {
+	const url = new URL(request.url);
+	const path = url.pathname.replace(/^\/agents/, "");
+	const proxyUrl = `https://server.s-ojling.workers.dev${path}${url.search}`;
+
+	// Handle WebSocket upgrade
+	if (request.headers.get("upgrade") === "websocket") {
+		const wsRequest = new Request(proxyUrl, request);
+		const response = await fetch(wsRequest);
+		return response;
+	}
+
+	// Proxy HTTP request
+	const proxyRequest = new Request(proxyUrl, request);
+	const response = await fetch(proxyRequest);
+	return response;
+};
